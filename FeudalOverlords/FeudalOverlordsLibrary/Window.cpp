3bc463bdc091dd5e88a899932cad2699320dc6a3
debiantarte <debiantarte@minet.net>
2019-06-10 23:17:50 +0200

    labels now display correctly

--- FeudalOverlords/FeudalOverlordsLibrary/Window.cpp
+++ FeudalOverlords/FeudalOverlordsLibrary/Window.cpp
@@ -13,29 +13,27 @@ Window::~Window()
 {
 }
 
-void Window::buildGUI(tgui::Gui& gui, sf::Texture& selectTex, sf::Texture& targetTex, string selTrp, string tgtTrp, string currentPlayer/)
+void Window::buildGUI(tgui::Gui& gui, sf::Texture& selectTex, sf::Texture& targetTex, string selTrp, string tgtTrp, string currentPlayer)
 {
 	sf::Texture uitex;
 	assert(uitex.loadFromFile("../../Assets/Textures/UItex.png"));
 	sf::Sprite background(uitex);
 	background.setScale((float)dimensions.first / uitex.getSize().x, ((float)dimensions.second / 7) / uitex.getSize().y);
 
-	sf::Text selectText{ selTrp, *gui.getFont(), 12};
-	selectText.setPosition(142, 20);
-	selectText.setFillColor({ 200, 200, 200 });
-
-	sf::Text targetText{ selTrp, *gui.getFont(), 12 };
-	targetText.setPosition(420, 20);
-	selectText.setFillColor({ 200, 200, 200 });
-
 	auto back_canvas = tgui::Canvas::create({ (float)dimensions.first, (float)dimensions.second / 7 });
 	back_canvas->setPosition(0.0, (float)dimensions.second * 6 / 7);
 	back_canvas->clear();
 	back_canvas->draw(background);
-	back_canvas->draw(selectText);
-	back_canvas->draw(targetText);
 	back_canvas->display();
 	gui.add(back_canvas);
+
+	auto selLabel = tgui::Label::create(selTrp);
+	selLabel->setPosition((float)dimensions.first * 0.177, (float)dimensions.second * 0.88);
+	gui.add(selLabel, "selection label");
+
+	auto tgtLabel = tgui::Label::create(tgtTrp);
+	tgtLabel->setPosition((float)dimensions.first * 0.530, (float)dimensions.second * 0.88);
+	gui.add(tgtLabel, "target label");
 
 	sf::Sprite selection(selectTex);
 	selection.setScale(1.07 * (float)dimensions.first / 600, 1.04 * (float)dimensions.second / 699);
@@ -51,8 +49,7 @@ void Window::buildGUI(tgui::Gui& gui, sf::Texture& selectTex, sf::Texture& targe
 	targetSpr.setScale(1.07 * (float)dimensions.first / 600, 1.04 * (float)dimensions.second / 699);
 
 	auto currentPlayerLabel = tgui::Label::create(currentPlayer + " turn");
-	currentPlayerLabel->setPosition((float)dimensions.first * 0.175, (float)dimensions.second * 0.88);
-	//currentPlayerLabel->draw();
+	currentPlayerLabel->setPosition((float)dimensions.first * 0.495, (float)dimensions.second * 0.86);
 	gui.add(currentPlayerLabel, "currentPlayer");
 
 	auto target_canvas = tgui::Canvas::create({ 64 * 1.07 * (float)dimensions.first / 600, 65 * 1.04 * (float)dimensions.second / 699 });
@@ -61,10 +58,10 @@ void Window::buildGUI(tgui::Gui& gui, sf::Texture& selectTex, sf::Texture& targe
 	target_canvas->draw(targetSpr);
 	target_canvas->display();
 	gui.add(target_canvas, "targetCanvas");
-
+	/*
 	auto button = tgui::Button::create();
 	button->setPosition(dimensions.first/2.0 - 50.0, (dimensions.second * 6.0 / 7.0) + 0.5 * (dimensions.second / 7) - 15);
 	button->setText("OK");
 	button->setSize(100, 30);
-	//button->connect("pressed", [=]() { gm->moveTroops(selected, target); });
+	*/
 }
